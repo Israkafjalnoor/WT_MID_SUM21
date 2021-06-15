270f7701d7bc95699066315ed597a812cce099c2
israk afjal noor <45044742+Israkafjalnoor@users.noreply.github.com>
2021-06-15 23:46:25 +0600

    php

---
+++ PHP/php3.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<title>p3</title>
+<?php
+$length=25;
+$width=10;
+$perimeter=2*($length+$width);
+$area=$length*$width;
+echo "area=".$area;
+?>
+</body>
+</html>
+
+
+
---
+++ PHP/php4.php
@@ -0,0 +1,30 @@
+<?php
+
+$backcolor="black";
+$fontcolor="blue";
+$imageWidth="300px";
+$imageHeight="300px";
+?>
+
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>P5</title>
+</head>
+<body>
+<table>
+<tr>
+<td>
+<img src="Pictures" height="<?php echo $imageHeight; ?>" width="<?php echo $imageWidth; ?>" >
+
+</td>
+
+
+<td>
+<span ><h2 style="background-color:<?php echo $backcolor;?>; color: <?php echo $fontcolor; ?>;">American International University-Bangladesh </h2></span>
+</td>
+</tr>
+</table>
+</body>
+</html>
